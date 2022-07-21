e706a4bd4a12da3c0422673e041c0ba79f31c175
hezex <89002817+hezex@users.noreply.github.com>
2022-07-21 12:39:14 +0800

    Other files
    Files generated by compiler

---
+++ hep_1_private.h
@@ -0,0 +1,23 @@
+/* THIS FILE WILL BE OVERWRITTEN BY DEV-C++ */
+/* DO NOT EDIT ! */
+
+#ifndef HEP_1_PRIVATE_H
+#define HEP_1_PRIVATE_H
+
+/* VERSION DEFINITIONS */
+#define VER_STRING	"1.0.1.3"
+#define VER_MAJOR	1
+#define VER_MINOR	0
+#define VER_RELEASE	1
+#define VER_BUILD	3
+#define COMPANY_NAME	""
+#define FILE_VERSION	"1.0.1.3"
+#define FILE_DESCRIPTION	"Developed using the Dev-C++ IDE"
+#define INTERNAL_NAME	"hep"
+#define LEGAL_COPYRIGHT	"Charles He"
+#define LEGAL_TRADEMARKS	""
+#define ORIGINAL_FILENAME	"hep"
+#define PRODUCT_NAME	"hep"
+#define PRODUCT_VERSION	"1.0.1.3"
+
+#endif /*HEP_1_PRIVATE_H*/
